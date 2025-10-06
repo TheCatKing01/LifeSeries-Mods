460c51e7c99861c5400acc605d2f3325cc8f296b
TheCatKing <dylansheath@outlook.com>
2025-10-06 17:42:25 +0100

    just config changes

--- src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/SimpleLife.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/SimpleLife.java
@@ -1,13 +1,9 @@
 package net.mat0u5.lifeseries.seasons.season.aprilfools.simplelife;
 
 import net.mat0u5.lifeseries.config.ConfigManager;
-import net.mat0u5.lifeseries.config.DefaultConfigValues;
 import net.mat0u5.lifeseries.seasons.season.Seasons;
 import net.mat0u5.lifeseries.seasons.season.thirdlife.ThirdLife;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityType;
-import net.minecraft.entity.SpawnReason;
-import net.minecraft.entity.SpawnRestriction;
+import net.minecraft.entity.*;
 import net.minecraft.entity.passive.TraderLlamaEntity;
 import net.minecraft.entity.passive.WanderingTraderEntity;
 import net.minecraft.entity.player.PlayerEntity;
@@ -29,9 +25,8 @@ import java.util.Optional;
 import java.util.Random;
 
 public class SimpleLife extends ThirdLife {
-    private final Random rnd = new Random();
+    private Random rnd = new Random();
     private int checkCooldown = 0;
-
     @Override
     public Seasons getSeason() {
         return Seasons.SIMPLE_LIFE;
@@ -45,94 +40,65 @@ public class SimpleLife extends ThirdLife {
     @Override
     public void tick(MinecraftServer server) {
         super.tick(server);
-
-        // Access the config
-        ConfigManager config = ConfigManager.get();
-        DefaultConfigValues defaults = new DefaultConfigValues();
-
-        // Read config values
-        boolean enableSimpleLife = config.get(defaults.SIMPLE_LIFE);
-        boolean complexTrades = config.get(defaults.SIMPLE_LIFE_COMPLEX_TRADES);
-
-        // If the main Simple Life toggle is off, skip
-        if (!enableSimpleLife) return;
-
         checkCooldown--;
         if (checkCooldown <= 0) {
-            checkCooldown = 1200; // 1 minute
+            checkCooldown = 1200; //1 Minute
             ServerWorld world = server.getOverworld();
             if (world == null) return;
-
             int traderCount = 0;
             for (Entity entity : world.iterateEntities()) {
                 if (entity instanceof WanderingTraderEntity) {
                     traderCount++;
                 }
             }
-
-            if (traderCount == 0) checkCooldown = 1200;
-            if (traderCount == 1) checkCooldown = 3600;
+            if (traderCount == 0) checkCooldown = 1200; //1 Minute
+            if (traderCount == 1) checkCooldown = 3600; //3 Minutes
             if (traderCount >= 2) checkCooldown = 200;
             if (traderCount >= 3) return;
-
             for (int i = 0; i < 5; i++) {
-                if (trySpawnTrader(world, complexTrades)) {
+                if (trySpawnTrader(world)) {
                     break;
                 }
             }
         }
     }
 
-    public boolean trySpawnTrader(ServerWorld world, boolean complexTrades) {
+    public boolean trySpawnTrader(ServerWorld world) {
         PlayerEntity playerEntity = world.getRandomAlivePlayer();
         if (playerEntity == null) {
             return true;
         } else {
             BlockPos blockPos = playerEntity.getBlockPos();
             PointOfInterestStorage pointOfInterestStorage = world.getPointOfInterestStorage();
-            Optional<BlockPos> optional = pointOfInterestStorage.getPosition(
-                    poiType -> poiType.matchesKey(PointOfInterestTypes.MEETING),
-                    pos -> true,
-                    blockPos,
-                    64,
-                    PointOfInterestStorage.OccupationStatus.ANY
-            );
-
-            BlockPos blockPos2 = optional.orElse(blockPos);
+            Optional<BlockPos> optional = pointOfInterestStorage.getPosition((poiType) -> {
+                return poiType.matchesKey(PointOfInterestTypes.MEETING);
+            }, (pos) -> {
+                return true;
+            }, blockPos, 64, PointOfInterestStorage.OccupationStatus.ANY);
+            BlockPos blockPos2 = (BlockPos)optional.orElse(blockPos);
             BlockPos blockPos3 = this.getNearbySpawnPos(world, blockPos2, 64);
             if (blockPos3 != null && this.doesNotSuffocateAt(world, blockPos3)) {
-                WanderingTraderEntity wanderingTraderEntity =
-                        (WanderingTraderEntity) EntityType.WANDERING_TRADER.spawn(world, blockPos3, SpawnReason.EVENT);
-
+                WanderingTraderEntity wanderingTraderEntity = (WanderingTraderEntity) EntityType.WANDERING_TRADER.spawn(world, blockPos3, SpawnReason.EVENT);
                 if (wanderingTraderEntity != null) {
-                    for (int j = 0; j < 2; ++j) {
+                    for(int j = 0; j < 2; ++j) {
                         this.spawnLlama(world, wanderingTraderEntity, 4);
                     }
 
                     wanderingTraderEntity.setDespawnDelay(12000);
 
-                    // Create trade offers based on config
-                    TradeOfferList offers = new TradeOfferList();
+                    TradeOfferList offers = wanderingTraderEntity.getOffers();
+                    offers.clear();
+                    offers.add(new TradeOffer(new TradedItem(Items.DIRT, 5), Optional.empty(), Items.IRON_INGOT.getDefaultStack(), 0, 999999, 0, 0, 0));
+                    offers.add(new TradeOffer(new TradedItem(Items.DIRT, 40), Optional.empty(), Items.WATER_BUCKET.getDefaultStack(), 0, 999999, 0, 0, 0));
+                    offers.add(new TradeOffer(new TradedItem(Items.DIRT, 40), Optional.empty(), Items.LAVA_BUCKET.getDefaultStack(), 0, 999999, 0, 0, 0));
+                    offers.add(new TradeOffer(new TradedItem(Items.DIRT, 1), Optional.empty(), Items.SAND.getDefaultStack(), 0, 999999, 0, 0, 0));
+                    offers.add(new TradeOffer(new TradedItem(Items.DIRT, 1), Optional.empty(), Items.GRAVEL.getDefaultStack(), 0, 999999, 0, 0, 0));
+                    offers.add(new TradeOffer(new TradedItem(Items.DIRT, 5), Optional.empty(), Items.GOLD_INGOT.getDefaultStack(), 0, 999999, 0, 0, 0));
+                    offers.add(new TradeOffer(new TradedItem(Items.DIRT, 1), Optional.empty(), Items.REDSTONE.getDefaultStack(), 0, 999999, 0, 0, 0));
 
-                    if (!complexTrades) {
-                        // Default Simple Life trades
-                        offers.add(new TradeOffer(new TradedItem(Items.DIRT, 5), Optional.empty(), Items.IRON_INGOT.getDefaultStack(), 0, 999999, 0, 0, 0));
-                        offers.add(new TradeOffer(new TradedItem(Items.DIRT, 40), Optional.empty(), Items.WATER_BUCKET.getDefaultStack(), 0, 999999, 0, 0, 0));
-                        offers.add(new TradeOffer(new TradedItem(Items.DIRT, 40), Optional.empty(), Items.LAVA_BUCKET.getDefaultStack(), 0, 999999, 0, 0, 0));
-                        offers.add(new TradeOffer(new TradedItem(Items.DIRT, 1), Optional.empty(), Items.SAND.getDefaultStack(), 0, 999999, 0, 0, 0));
-                        offers.add(new TradeOffer(new TradedItem(Items.DIRT, 1), Optional.empty(), Items.GRAVEL.getDefaultStack(), 0, 999999, 0, 0, 0));
-                        offers.add(new TradeOffer(new TradedItem(Items.DIRT, 5), Optional.empty(), Items.GOLD_INGOT.getDefaultStack(), 0, 999999, 0, 0, 0));
-                        offers.add(new TradeOffer(new TradedItem(Items.DIRT, 1), Optional.empty(), Items.REDSTONE.getDefaultStack(), 0, 999999, 0, 0, 0));
-                    } else {
-                        // Complex Life trades (alternate set)
-                        offers.add(new TradeOffer(new TradedItem(Items.STONE, 16), Optional.empty(), Items.DIAMOND.getDefaultStack(), 0, 999999, 0, 0, 0));
-                        offers.add(new TradeOffer(new TradedItem(Items.GRAVEL, 20), Optional.empty(), Items.EMERALD.getDefaultStack(), 0, 999999, 0, 0, 0));
-                        offers.add(new TradeOffer(new TradedItem(Items.COBBLESTONE, 40), Optional.empty(), Items.NETHERITE_SCRAP.getDefaultStack(), 0, 999999, 0, 0, 0));
-                        offers.add(new TradeOffer(new TradedItem(Items.STONE, 10), Optional.empty(), Items.WATER_BUCKET.getDefaultStack(), 0, 999999, 0, 0, 0));
-                        offers.add(new TradeOffer(new TradedItem(Items.STONE, 1), Optional.empty(), Items.IRON_INGOT.getDefaultStack(), 0, 999999, 0, 0, 0));
-                        offers.add(new TradeOffer(new TradedItem(Items.STONE, 5), Optional.empty(), Items.REDSTONE_BLOCK.getDefaultStack(), 0, 999999, 0, 0, 0));
-                        offers.add(new TradeOffer(new TradedItem(Items.STONE, 2), Optional.empty(), Items.EXPERIENCE_BOTTLE.getDefaultStack(), 0, 999999, 0, 0, 0));
-                    }
+                    int rand = rnd.nextInt(2);
+                    if (rand == 0) offers.add(new TradeOffer(new TradedItem(Items.DIRT, 32), Optional.empty(), Items.OAK_SAPLING.getDefaultStack(), 0, 999999, 0, 0, 0));
+                    if (rand == 1) offers.add(new TradeOffer(new TradedItem(Items.DIRT, 32), Optional.empty(), Items.SPRUCE_SAPLING.getDefaultStack(), 0, 999999, 0, 0, 0));
 
                     wanderingTraderEntity.setOffersFromServer(offers);
                     return true;
@@ -146,8 +112,7 @@ public class SimpleLife extends ThirdLife {
     private void spawnLlama(ServerWorld world, WanderingTraderEntity wanderingTrader, int range) {
         BlockPos blockPos = this.getNearbySpawnPos(world, wanderingTrader.getBlockPos(), range);
         if (blockPos != null) {
-            TraderLlamaEntity traderLlamaEntity =
-                    (TraderLlamaEntity) EntityType.TRADER_LLAMA.spawn(world, blockPos, SpawnReason.EVENT);
+            TraderLlamaEntity traderLlamaEntity = (TraderLlamaEntity)EntityType.TRADER_LLAMA.spawn(world, blockPos, SpawnReason.EVENT);
             if (traderLlamaEntity != null) {
                 traderLlamaEntity.attachLeash(wanderingTrader, true);
             }
@@ -156,9 +121,9 @@ public class SimpleLife extends ThirdLife {
 
     private BlockPos getNearbySpawnPos(WorldView world, BlockPos pos, int range) {
         BlockPos blockPos = null;
-        SpawnRestriction.Location spawnLocation = SpawnRestriction.getLocation(EntityType.WANDERING_TRADER);
+        SpawnLocation spawnLocation = SpawnRestriction.getLocation(EntityType.WANDERING_TRADER);
 
-        for (int i = 0; i < 10; ++i) {
+        for(int i = 0; i < 10; ++i) {
             int j = pos.getX() + rnd.nextInt(range * 2) - range;
             int k = pos.getZ() + rnd.nextInt(range * 2) - range;
             int l = world.getTopY(Heightmap.Type.WORLD_SURFACE, j, k);
@@ -173,14 +138,17 @@ public class SimpleLife extends ThirdLife {
     }
 
     private boolean doesNotSuffocateAt(BlockView world, BlockPos pos) {
-        Iterator<BlockPos> it = BlockPos.iterate(pos, pos.add(1, 2, 1)).iterator();
+        Iterator var3 = BlockPos.iterate(pos, pos.add(1, 2, 1)).iterator();
 
-        while (it.hasNext()) {
-            BlockPos checkPos = it.next();
-            if (!world.getBlockState(checkPos).getCollisionShape(world, checkPos).isEmpty()) {
-                return false;
+        BlockPos blockPos;
+        do {
+            if (!var3.hasNext()) {
+                return true;
             }
-        }
-        return true;
+
+            blockPos = (BlockPos)var3.next();
+        } while(world.getBlockState(blockPos).getCollisionShape(world, blockPos).isEmpty());
+
+        return false;
     }
 }
