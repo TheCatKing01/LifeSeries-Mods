acb51773187a455e1ed73232534c2e203cd8819e
TheCatKing <dylansheath@outlook.com>
2025-11-15 22:01:33 +0000

    super life mini

--- src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/wildcards/wildcard/superpowers/SuperpowersWildcard.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/wildcards/wildcard/superpowers/SuperpowersWildcard.java
@@ -72,7 +72,6 @@ public class SuperpowersWildcard extends Wildcard {
     }
 
     public static void rollRandomSuperpowers() {
-        resetAllSuperpowers();
         List<Superpowers> implemented = new ArrayList<>(Superpowers.getImplemented());
         blacklistedPowers.forEach(implemented::remove);
 
