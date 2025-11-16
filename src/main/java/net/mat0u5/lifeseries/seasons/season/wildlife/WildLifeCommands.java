72e7cacd01c5322c8dca9e78f62cc02b3c0eb71c
TheCatKing <dylansheath@outlook.com>
2025-11-16 16:52:56 +0000

    Update WildLifeCommands.java

--- src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/WildLifeCommands.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/WildLifeCommands.java
@@ -146,7 +146,7 @@ public class WildLifeCommands extends Command {
                                 .executes(context -> resetSuperpowers(context.getSource(), EntityArgument.getPlayers(context, "player")))
                         )
                 )
-                .then(literal("addRandom")
+                .then(literal("rollPowers")
                     .executes(context -> setRandomSuperpowers(context.getSource()))
                 )
                 .then(literal("getMostRecent")
