626cf6ae86c7519d7c7f69b1aab63e8aa3dedd2b
TheCatKing <dylansheath@outlook.com>
2025-11-19 17:09:16 +0000

    more changes

--- src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/WildLifeCommands.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/WildLifeCommands.java
@@ -161,17 +161,7 @@ public class WildLifeCommands extends Command {
                 .then(literal("skipCooldown")
                     .executes(context -> skipSuperpowerCooldown(context.getSource()))
                 )
-                .then(literal("force")
-                    .then(argument("player", EntityArgument.players())
-                        .then(argument("superpower", StringArgumentType.string())
-                            .suggests((context, builder) -> SharedSuggestionProvider.suggest(Superpowers.getImplementedStr(), builder))
-                            .executes(context -> assignSuperpower(context.getSource(), EntityArgument.getPlayers(context, "player"), StringArgumentType.getString(context, "superpower")))
-                        )
-                        .then(literal("reset")
-                                .executes(context -> assignSuperpower(context.getSource(), EntityArgument.getPlayers(context, "player"), null))
-                        )
-                    )
-                )
+
         );
                 
         dispatcher.register(
