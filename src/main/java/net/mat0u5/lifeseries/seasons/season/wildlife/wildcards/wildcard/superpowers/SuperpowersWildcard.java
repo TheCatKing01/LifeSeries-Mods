2ade78d334b9560424007d6d453b143e8c996218
TheCatKing <dylansheath@outlook.com>
2025-11-21 16:18:47 +0000

    Revert "Update SuperpowersWildcard.java"
    This reverts commit dd865c76dd1186e12dda442b8140c6783664ea00.

--- src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/wildcards/wildcard/superpowers/SuperpowersWildcard.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/wildcards/wildcard/superpowers/SuperpowersWildcard.java
@@ -27,7 +27,7 @@ public class SuperpowersWildcard extends Wildcard {
     public static int ZOMBIES_HEALTH = 8;
 	
 	public static boolean POWER_STACKING_ENABLED() {
-    return Main.WILDLIFE_CONFIG.WILDCARD_CALLBACK_POWER_STACKING.get(Main.WILDLIFE_CONFIG);
+    return WildLifeConfig.WILDCARD_CALLBACK_POWER_STACKING.get();
 	
 	}
 
