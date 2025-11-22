4be60a0fc12bc5506da78fab8e791cb9ca32df28
TheCatKing <dylansheath@outlook.com>
2025-11-22 20:14:31 +0000

    Revert "secret - (if this worked - WOW)"
    This reverts commit 0482c48024170ad4ed476c9ce03b3d7efc1ca99e.

--- src/main/java/net/mat0u5/lifeseries/registries/ModRegistries.java
+++ src/main/java/net/mat0u5/lifeseries/registries/ModRegistries.java
@@ -11,9 +11,9 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.levelgen.presets.WorldPreset;
 
 public class ModRegistries {
-	public static final ResourceKey<WorldPreset> SMALL = ResourceKey.create(
+	public static final ResourceKey<WorldPreset> SMALL_BIOMES = ResourceKey.create(
             Registries.WORLD_PRESET,
-            IdentifierHelper.mod("small")
+            IdentifierHelper.mod("small_biomes")
     );
 	
 	public static final ResourceKey<WorldPreset> SIMPLE = ResourceKey.create(
