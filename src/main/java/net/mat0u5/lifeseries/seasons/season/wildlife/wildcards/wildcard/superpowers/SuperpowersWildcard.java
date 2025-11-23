6ba6475e229f0b20aada215110e7498ddd0e01dd
TheCatKing <dylansheath@outlook.com>
2025-11-23 10:43:09 +0000

    maybe i'm less smart

--- src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/wildcards/wildcard/superpowers/SuperpowersWildcard.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/wildcards/wildcard/superpowers/SuperpowersWildcard.java
@@ -49,7 +49,7 @@ public class SuperpowersWildcard extends Wildcard {
 
     @Override
     public void deactivate() {
-		if (WILDCARD_SUPERPOWERS_POWER_STACKING) {
+		if (WILDCARD_CALLBACK_POWER_STACKING) {
 			    super.deactivate();
 		}
         super.deactivate();
