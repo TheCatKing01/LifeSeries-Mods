a30c7a232c12b9d265a12cc09c29a9e49982851e
TheCatKing <dylansheath@outlook.com>
2025-11-23 16:19:24 +0000

    Update to 1.4.4

--- src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/WildLifeConfig.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/WildLifeConfig.java
@@ -107,7 +107,7 @@ public class WildLifeConfig extends ConfigManager {
             "Play Sound Chance", "Chance for food to play a random sound to everyone on the server."
     );
     public static final ConfigFileEntry<String> WILDCARD_HUNGER_NON_EDIBLE_ITEMS = new ConfigFileEntry<>(
-            "wildcard_hunger_non_edible_items", "[]", ConfigTypes.ITEM_LIST, "season.hunger[new]",
+            "wildcard_hunger_non_edible_items", "[]", ConfigTypes.ITEM_LIST, "season.hunger",
             "Non Edible Itmes", "A list of items that you can't eat."
     );
 
@@ -188,7 +188,7 @@ public class WildLifeConfig extends ConfigManager {
             "Necromancy: Zombies Can Revive", "Controls whether zombies can be revived (gain a life) by killing a dark green player."
     );
     public static final ConfigFileEntry<Integer> WILDCARD_SUPERPOWERS_ZOMBIES_HEALTH = new ConfigFileEntry<>(
-            "wildcard_superpowers_zombies_health", 8, "season.superpowers[new]",
+            "wildcard_superpowers_zombies_health", 8, "season.superpowers",
             "Necromancy: Zombie Health Amount", "Controls how much health zombies will have."
     );
     public static final ConfigFileEntry<Boolean> WILDCARD_SUPERPOWERS_SUPERSPEED_STEP = new ConfigFileEntry<>(
@@ -227,7 +227,7 @@ public class WildLifeConfig extends ConfigManager {
     );
 	
 		    public static final ConfigFileEntry<Boolean> WILDCARD_CALLBACK_POWER_STACKING = new ConfigFileEntry<>(
-            "wildcard_callback_power_stacking", false, "season.callback[custom]",
+            "wildcard_callback_power_stacking", false, "season.callback[new]",
             "Power Stacking", "Instead of the superpower wildcard deactivating, the powers stay allowing stacking when the superpower wildcard activates again."
     );
 
