ff272e8b41a93e7aa4c942195cf60da7b289ae76
TheCatKing <dylansheath@outlook.com>
2025-12-02 19:02:11 +0000

    Update WandingTraders.java

--- src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/WandingTraders.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/WandingTraders.java
@@ -68,11 +68,10 @@ public class WandingTraders {
         for (Entity e : level.getAllEntities()) {
             if (e instanceof WanderingTrader) traderCount++;
         }
-
-        if (traderCount == 0)       checkCooldown = 1200;
-        else if (traderCount == 1) checkCooldown = 3600;
-        else if (traderCount == 2) checkCooldown = 200;
-        else return; // traderCount >= 3
+		
+		if (traderCount == 0) checkCooldown = 1200; // 1 minute
+		if (traderCount >= 1) checkCooldown = 1800; // 1.5 minutes
+		if (traderCount >= TRADERS_MAX_AMOUNT) return;
 
         for (int i = 0; i < 5; i++) {
             if (trySpawnTrader(level)) break;
@@ -115,37 +114,66 @@ public class WandingTraders {
         // custom trades
         MerchantOffers offers = trader.getOffers();
         offers.clear();
+		
+		if (!COMPLEX_LIFE_TRADES) {
+			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(),
+					Items.IRON_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-        offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(),
-                Items.IRON_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
+			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(),
+					Items.WATER_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-        offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(),
-                Items.WATER_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
+			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(),
+					Items.LAVA_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-        offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(),
-                Items.LAVA_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
+			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(),
+					Items.SAND.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-        offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(),
-                Items.SAND.getDefaultInstance(), 0, 999999, 0, 0, 0));
+			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(),
+					Items.GRAVEL.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-        offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(),
-                Items.GRAVEL.getDefaultInstance(), 0, 999999, 0, 0, 0));
+			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(),
+					Items.GOLD_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-        offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(),
-                Items.GOLD_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
+			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(),
+					Items.REDSTONE.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-        offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(),
-                Items.REDSTONE.getDefaultInstance(), 0, 999999, 0, 0, 0));
+			int rand = rnd.nextInt(2);
+			if (rand == 0) {
+				offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(),
+						Items.OAK_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
+			} else {
+				offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(),
+						Items.SPRUCE_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
+			}
+		}
+		
+		else{
+		
+				offers.add(new TradeOffer(new TradedItem(Items.DIRT, 32), Optional.empty(), Items.OAK_SAPLING.getDefaultStack(), 0, 999999, 0, 0, 0));
+				offers.add(new TradeOffer(new TradedItem(Items.DIRT, 5), Optional.empty(), Items.BONE.getDefaultStack(), 0, 999999, 0, 0, 0));
+				offers.add(new TradeOffer(new TradedItem(Items.DIRT, 5), Optional.empty(), Items.SAND.getDefaultStack(), 0, 999999, 0, 0, 0));
 
-        int rand = rnd.nextInt(2);
-        if (rand == 0) {
-            offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(),
-                    Items.OAK_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
-        } else {
-            offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(),
-                    Items.SPRUCE_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
-        }
+				offers.add(new TradeOffer(new TradedItem(Items.SAND, 10), Optional.empty(), Items.SUGAR_CANE.getDefaultStack(), 0, 999999, 0, 0, 0));
 
+				offers.add(new TradeOffer(new TradedItem(Items.OAK_PLANKS, 2), Optional.empty(), Items.COBBLESTONE.getDefaultStack(), 0, 999999, 0, 0, 0));
+				offers.add(new TradeOffer(new TradedItem(Items.OAK_PLANKS, 40), Optional.empty(), Items.WATER_BUCKET.getDefaultStack(), 0, 999999, 0, 0, 0));
+				offers.add(new TradeOffer(new TradedItem(Items.OAK_PLANKS, 40), Optional.empty(), Items.LAVA_BUCKET.getDefaultStack(), 0, 999999, 0, 0, 0));
+				offers.add(new TradeOffer(new TradedItem(Items.OAK_PLANKS, 32), Optional.empty(), Items.COW_SPAWN_EGG.getDefaultStack(), 0, 999999, 0, 0, 0));
+
+				offers.add(new TradeOffer(new TradedItem(Items.COBBLESTONE, 5), Optional.empty(), Items.IRON_INGOT.getDefaultStack(), 0, 999999, 0, 0, 0));
+				offers.add(new TradeOffer(new TradedItem(Items.COBBLESTONE, 10), Optional.empty(), Items.GOLD_INGOT.getDefaultStack(), 0, 999999, 0, 0, 0));
+				offers.add(new TradeOffer(new TradedItem(Items.COBBLESTONE, 1), Optional.empty(), Items.REDSTONE.getDefaultStack(), 0, 999999, 0, 0, 0));
+				offers.add(new TradeOffer(new TradedItem(Items.COBBLESTONE, 1), Optional.empty(), Items.GRAVEL.getDefaultStack(), 0, 999999, 0, 0, 0));
+
+				offers.add(new TradeOffer(new TradedItem(Items.IRON_INGOT, 10), Optional.empty(), Items.DIAMOND.getDefaultStack(), 0, 999999, 0, 0, 0));
+				offers.add(new TradeOffer(new TradedItem(Items.IRON_INGOT, 40), Optional.empty(), Items.TRIDENT.getDefaultStack(), 0, 999999, 0, 0, 0));
+				offers.add(new TradeOffer(new TradedItem(Items.IRON_INGOT, 16), Optional.empty(), Items.WOLF_SPAWN_EGG.getDefaultStack(), 0, 999999, 0, 0, 0));
+				offers.add(new TradeOffer(new TradedItem(Items.IRON_INGOT, 40), Optional.empty(), Items.TRIDENT.getDefaultStack(), 0, 999999, 0, 0, 0));
+
+				offers.add(new TradeOffer(new TradedItem(Items.GOLD_INGOT, 5), Optional.empty(), Items.LAPIS_LAZULI.getDefaultStack(), 0, 999999, 0, 0, 0));
+
+		}
+		
         trader.overrideOffers(offers);
         trader.addTag("SimpleLifeTrader");
 
