96d2d0db3c6ae21c6f61d197cb00cb898c386bca
TheCatKing <dylansheath@outlook.com>
2025-12-04 19:54:37 +0000

    test fix config (probably wont work again)

--- src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/WandingTraders.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/WandingTraders.java
@@ -16,7 +16,6 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.levelgen.Heightmap;
 
-import java.util.Iterator;
 import java.util.Optional;
 import java.util.Random;
 
@@ -39,23 +38,23 @@ import static net.mat0u5.lifeseries.Main.*;
 
 public class WandingTraders {
 
-    public boolean SIMPLE_LIFE = false;
-    public int TRADERS_MAX_AMOUNT = 3;
-    public boolean COMPLEX_LIFE_TRADES = false;
-
-    public void onReload() {
-
-        SIMPLE_LIFE = seasonConfig.SIMPLE_LIFE.get(seasonConfig);
-        TRADERS_MAX_AMOUNT = seasonConfig.TRADERS_MAX_AMOUNT.get(seasonConfig);
-        COMPLEX_LIFE_TRADES = seasonConfig.COMPLEX_LIFE_TRADES.get(seasonConfig);
-    }
-
     private final Random rnd = new Random();
     private int checkCooldown = 0;
 
-    public void tickSessionOn(MinecraftServer server) {
+    private boolean isSimpleLife() {
+        return seasonConfig.SIMPLE_LIFE.get(seasonConfig);
+    }
 
-        if (!SIMPLE_LIFE) return;
+    private int getTradersMaxAmount() {
+        return seasonConfig.TRADERS_MAX_AMOUNT.get(seasonConfig);
+    }
+
+    private boolean isComplexLifeTrades() {
+        return seasonConfig.COMPLEX_LIFE_TRADES.get(seasonConfig);
+    }
+
+    public void tickSessionOn(MinecraftServer server) {
+        if (!isSimpleLife()) return;
 
         checkCooldown--;
 
@@ -68,10 +67,10 @@ public class WandingTraders {
         for (Entity e : level.getAllEntities()) {
             if (e instanceof WanderingTrader) traderCount++;
         }
-		
-		if (traderCount == 0) checkCooldown = 1200; // 1 minute
-		if (traderCount >= 1) checkCooldown = 1800; // 1.5 minutes
-		if (traderCount >= TRADERS_MAX_AMOUNT) return;
+
+        if (traderCount == 0) checkCooldown = 1200; // 1 minute
+        if (traderCount >= 1) checkCooldown = 1800; // 1.5 minutes
+        if (traderCount >= getTradersMaxAmount()) return;
 
         for (int i = 0; i < 5; i++) {
             if (trySpawnTrader(level)) break;
@@ -79,7 +78,6 @@ public class WandingTraders {
     }
 
     public boolean trySpawnTrader(ServerLevel level) {
-
         Player player = level.getRandomPlayer();
         if (player == null) return true;
 
@@ -99,71 +97,59 @@ public class WandingTraders {
         if (spawnPos == null) return false;
         if (!doesNotSuffocateAt(level, spawnPos)) return false;
 
-        WanderingTrader trader =
-                LevelUtils.spawnEntity(EntityType.WANDERING_TRADER, level, spawnPos);
-
+        WanderingTrader trader = LevelUtils.spawnEntity(EntityType.WANDERING_TRADER, level, spawnPos);
         if (trader == null) return false;
 
-        // spawn llamas
         for (int i = 0; i < 2; i++) {
             spawnLlama(level, trader, 4);
         }
 
         trader.setDespawnDelay(12000);
 
-        // custom trades
         MerchantOffers offers = trader.getOffers();
         offers.clear();
-		
-		if (!COMPLEX_LIFE_TRADES) {
-			
-			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(),Items.IRON_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(),Items.WATER_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(),Items.LAVA_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(),Items.SAND.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(),Items.GRAVEL.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(),Items.GOLD_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(),Items.REDSTONE.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-			int rand = rnd.nextInt(2);
-			if (rand == 0) {
-				offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(),Items.OAK_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			} else {
-				offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(),Items.SPRUCE_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			}
-		}
-		
-		else{
-		
-			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(), Items.OAK_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(), Items.BONE.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(), Items.SAND.getDefaultInstance(), 0, 999999, 0, 0, 0));
+        if (!isComplexLifeTrades()) {
+            offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(), Items.IRON_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(), Items.WATER_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(), Items.LAVA_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(), Items.SAND.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(), Items.GRAVEL.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(), Items.GOLD_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(), Items.REDSTONE.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-			offers.add(new MerchantOffer(new ItemCost(Items.SAND, 10), Optional.empty(), Items.SUGAR_CANE.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            int rand = rnd.nextInt(2);
+            if (rand == 0) {
+                offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(), Items.OAK_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            } else {
+                offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(), Items.SPRUCE_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            }
+        } else {
+            offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(), Items.OAK_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(), Items.BONE.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(), Items.SAND.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.SAND, 10), Optional.empty(), Items.SUGAR_CANE.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.OAK_PLANKS, 2), Optional.empty(), Items.COBBLESTONE.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.OAK_PLANKS, 40), Optional.empty(), Items.WATER_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.OAK_PLANKS, 40), Optional.empty(), Items.LAVA_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.OAK_PLANKS, 32), Optional.empty(), Items.COW_SPAWN_EGG.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-			offers.add(new MerchantOffer(new ItemCost(Items.OAK_PLANKS, 2), Optional.empty(), Items.COBBLESTONE.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.OAK_PLANKS, 40), Optional.empty(), Items.WATER_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.OAK_PLANKS, 40), Optional.empty(), Items.LAVA_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.OAK_PLANKS, 32), Optional.empty(), Items.COW_SPAWN_EGG.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.COBBLESTONE, 1), Optional.empty(), Items.REDSTONE.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.COBBLESTONE, 1), Optional.empty(), Items.GRAVEL.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.COBBLESTONE, 5), Optional.empty(), Items.IRON_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.COBBLESTONE, 10), Optional.empty(), Items.GOLD_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-			offers.add(new MerchantOffer(new ItemCost(Items.COBBLESTONE, 1), Optional.empty(), Items.REDSTONE.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.COBBLESTONE, 1), Optional.empty(), Items.GRAVEL.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.COBBLESTONE, 5), Optional.empty(), Items.IRON_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.COBBLESTONE, 10), Optional.empty(), Items.GOLD_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.IRON_INGOT, 10), Optional.empty(), Items.DIAMOND.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.IRON_INGOT, 16), Optional.empty(), Items.WOLF_SPAWN_EGG.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.IRON_INGOT, 40), Optional.empty(), Items.TRIDENT.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-			offers.add(new MerchantOffer(new ItemCost(Items.IRON_INGOT, 10), Optional.empty(), Items.DIAMOND.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.IRON_INGOT, 16), Optional.empty(), Items.WOLF_SPAWN_EGG.getDefaultInstance(), 0, 999999, 0, 0, 0));
-			offers.add(new MerchantOffer(new ItemCost(Items.IRON_INGOT, 40), Optional.empty(), Items.TRIDENT.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            offers.add(new MerchantOffer(new ItemCost(Items.GOLD_INGOT, 5), Optional.empty(), Items.LAPIS_LAZULI.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
-			offers.add(new MerchantOffer(new ItemCost(Items.GOLD_INGOT, 5), Optional.empty(), Items.LAPIS_LAZULI.getDefaultInstance(), 0, 999999, 0, 0, 0));
-				
-			int rand = rnd.nextInt(2);
-				if (rand == 0) offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(), Items.OAK_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
-				if (rand == 1) offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(), Items.SPRUCE_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            int rand = rnd.nextInt(2);
+            if (rand == 0) offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(), Items.OAK_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
+            if (rand == 1) offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(), Items.SPRUCE_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
+        }
 
-
-		}
-		
         trader.overrideOffers(offers);
         trader.addTag("SimpleLifeTrader");
 
@@ -185,7 +171,6 @@ public class WandingTraders {
             int y = world.getHeight(Heightmap.Types.WORLD_SURFACE, x, z);
 
             BlockPos tryPos = new BlockPos(x, y, z);
-            // NOTE: update if using new SpawnPlacements API
             if (SpawnPlacements.getPlacementType(EntityType.WANDERING_TRADER)
                     .isSpawnPositionOk(world, tryPos, EntityType.WANDERING_TRADER)) {
                 return tryPos;
