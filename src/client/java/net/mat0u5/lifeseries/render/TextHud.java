4cb5e702c5c1c394494079bc26bb201e3fd41b8d
TheCatKing <dylansheath@outlook.com>
2025-12-07 17:48:57 +0000

    Update TextHud.java

--- src/client/java/net/mat0u5/lifeseries/render/TextHud.java
+++ src/client/java/net/mat0u5/lifeseries/render/TextHud.java
@@ -12,6 +12,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
+import net.mat0u5.lifeseries.seasons.season.limitedlife.LimitedLife;
 
 public class TextHud {
 	
