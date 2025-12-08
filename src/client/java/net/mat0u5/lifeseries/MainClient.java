40ee97d8a841f0caeefdf31e8fd6ce7ca7fe9801
TheCatKing <dylansheath@outlook.com>
2025-12-08 16:27:45 +0000

    added ; oops

--- src/client/java/net/mat0u5/lifeseries/MainClient.java
+++ src/client/java/net/mat0u5/lifeseries/MainClient.java
@@ -56,7 +56,7 @@ public class MainClient implements ClientModInitializer, IClientHelper {
     public static boolean FIX_SIZECHANGING_BUGS = false;
     public static float SIZESHIFTING_CHANGE = 0;
     public static double TEXT_HUD_SCALE = 1.0;
-    public static int TICKS_PER_SECOND = 20
+    public static int TICKS_PER_SECOND = 20;
 
 
     public static boolean isReplay = false;
