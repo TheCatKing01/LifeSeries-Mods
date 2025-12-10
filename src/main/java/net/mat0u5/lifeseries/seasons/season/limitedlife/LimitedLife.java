645e4641f43df506f8fef044dc677d30d55bf5ef
TheCatKing <dylansheath@outlook.com>
2025-12-10 18:38:43 +0000

    Update LimitedLife.java

--- src/main/java/net/mat0u5/lifeseries/seasons/season/limitedlife/LimitedLife.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/limitedlife/LimitedLife.java
@@ -30,7 +30,7 @@ import java.util.Collection;
 /*import net.minecraft.world.scores.Score;*/
 //? if > 1.20.2
 import net.minecraft.world.scores.PlayerScoreEntry;
-=import net.minecraft.world.scores.Team;
+import net.minecraft.world.scores.Team;
 
 import static net.mat0u5.lifeseries.Main.*;
 
