9d1b30967f142b4ad4c01a171beebb812d5afd83
TheCatKing <dylansheath@outlook.com>
2025-12-10 20:57:08 +0000

    Update LimitedLife.java

--- src/main/java/net/mat0u5/lifeseries/seasons/season/limitedlife/LimitedLife.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/limitedlife/LimitedLife.java
@@ -23,17 +23,14 @@ import net.minecraft.world.damagesource.DamageSource;
 import net.mat0u5.lifeseries.seasons.season.aprilfools.simplelife.WandingTraders;
 
 //? if <= 1.20.2
-{
-    import net.minecraft.world.scores.Score;
-}
+import net.minecraft.world.scores.Score;
 //? else
-{
-    import net.minecraft.world.scores.PlayerScoreEntry;
-    import net.minecraft.world.scores.ScoreHolder;
-    import net.minecraft.world.scores.DisplaySlot;
-    import net.minecraft.world.scores.Team;
-}
+import net.minecraft.world.scores.PlayerScoreEntry;
+import net.minecraft.world.scores.ScoreHolder;
+import net.minecraft.world.scores.DisplaySlot;
+import net.minecraft.world.scores.Team;
 //?
+
 
 
 import static net.mat0u5.lifeseries.Main.*;
