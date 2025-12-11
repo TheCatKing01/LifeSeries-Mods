c46011d4caf0b137f05600abc04a9c6514fd6d13
TheCatKing <dylansheath@outlook.com>
2025-12-11 15:44:39 +0000

    ooops

--- src/main/java/net/mat0u5/lifeseries/seasons/season/limitedlife/LimitedLife.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/limitedlife/LimitedLife.java
@@ -27,7 +27,6 @@ import net.mat0u5.lifeseries.seasons.season.aprilfools.simplelife.WandingTraders
 import net.minecraft.world.scores.ScoreHolder;
 *///?} else {
 import net.minecraft.world.scores.PlayerScoreEntry;
-import net.minecraft.world.scores.ScoreHolder;
 import net.minecraft.world.scores.DisplaySlot;
 import net.minecraft.world.scores.Team;
 //? }
