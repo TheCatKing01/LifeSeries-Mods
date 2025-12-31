c82032dafe692dc8ce6cf1d9b06438c7fb6c6390
TheCatKing <dylansheath@outlook.com>
2025-12-31 20:58:02 +0000

    Revert "fix sounds"
    This reverts commit f714accf362d844f73bc37f1eb9bdd30e2cff08a.

--- src/main/java/net/mat0u5/lifeseries/utils/player/PlayerUtils.java
+++ src/main/java/net/mat0u5/lifeseries/utils/player/PlayerUtils.java
@@ -193,7 +193,7 @@ public class PlayerUtils {
                 id,
                 link,
                 sha1,
-                true,
+                false,
                 //? if <= 1.20.3 {
                 /*Component.translatable(message)
                 *///?} else {
