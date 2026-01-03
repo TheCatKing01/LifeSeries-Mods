34afd21aeda2118493d65c338fe6b53abaa8349d
TheCatKing <dylansheath@outlook.com>
2026-01-03 14:39:16 +0000

    blcoks

--- src/main/java/net/mat0u5/lifeseries/block/Blocks.java
+++ src/main/java/net/mat0u5/lifeseries/block/Blocks.java
@@ -3,44 +3,46 @@ package net.mat0u5.lifeseries.block;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.mat0u5.lifeseries.Main;
-import net.minecraft.block.Block;
-import net.minecraft.block.ExperienceDroppingBlock;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroups;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.math.intprovider.UniformIntProvider;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.DropExperienceBlock;
 
 public class Blocks {
 
     public static final Block SNOWY_GOLD_ORE = registerBlock("snowy_gold_ore",
-            new ExperienceDroppingBlock(
-                    FabricBlockSettings.copyOf(net.minecraft.block.Blocks.NETHER_GOLD_ORE),
-                    UniformIntProvider.create(0, 1)));
+            new DropExperienceBlock(
+                    FabricBlockSettings.copyOf(Blocks.NETHER_GOLD_ORE),
+                    UniformInt.of(0, 1)));
 					
     public static final Block SNOWY_QUARTZ_ORE = registerBlock("snowy_quartz_ore",
-            new ExperienceDroppingBlock(
-                    FabricBlockSettings.copyOf(net.minecraft.block.Blocks.NETHER_QUARTZ_ORE),
-                    UniformIntProvider.create(2, 5)));
+            new DropExperienceBlock(
+                    FabricBlockSettings.copyOf(Blocks.NETHER_QUARTZ_ORE),
+                    UniformInt.of(2, 5)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
-        return Registry.register(Registries.BLOCK, Identifier.of(Main.MOD_ID, name), block);
-    }
+        return net.minecraft.core.Registry.register(BuiltInRegistries.BLOCK,
+                ResourceLocation.fromNamespaceAndPath(Main.MOD_ID, name), block);
+	}
 
     private static void registerBlockItem(String name, Block block) {
-        Registry.register(Registries.ITEM, Identifier.of(Main.MOD_ID, name),
-                new BlockItem(block, new Item.Settings()));
-    };
+        net.minecraft.core.Registry.register(BuiltInRegistries.ITEM,
+                ResourceLocation.fromNamespaceAndPath(Main.MOD_ID, name),
+                new BlockItem(block, new Item.Properties()));
+    }
 
     public static void registerBlocks() {
-        lifeseries.LOGGER.info("Registering Blocks for" + Main.MOD_ID);
-
-        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> {
-            entries.add(SNOWY_GOLD_ORE);
-            entries.add(SNOWY_QUARTZ_ORE);
+        Main.LOGGER.info("Registering Blocks for " + Main.MOD_ID);
+		
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.NATURAL_BLOCKS).register(entries -> {
+            entries.accept(SNOWY_GOLD_ORE);
+            entries.accept(SNOWY_QUARTZ_ORE);
 		});
     }
 }
