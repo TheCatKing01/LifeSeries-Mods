738ade7be9166d0dd9a313c026a16e3ecb171406
TheCatKing <dylansheath@outlook.com>
2026-02-16 15:05:34 +0000

    Update WildLifeCommands.java

--- src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/WildLifeCommands.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/wildlife/WildLifeCommands.java
@@ -59,20 +59,58 @@ public class WildLifeCommands extends Command {
     public void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(
 		
-			literal("wildcard")
+		literal("wildcard")
 			.requires(PermissionManager::isAdmin)
-			.then(literal("list").executes(...))
-			.then(literal("listActive").executes(...))
-			.then(literal("activate").then(argument("wildcard", ...).executes(...)))
-			.then(literal("deactivate").then(argument("wildcard", ...).executes(...)))
-			.then(literal("choose").executes(...))
-			.then(literal("finale").executes(context -> activateFinale(context.getSource())))
+			
+			.then(literal("list")
+				.executes(context -> listWildcards(context.getSource()))
+			)
+			
+			.then(literal("listActive")
+				.executes(context -> listActiveWildcards(context.getSource()))
+			)
+			
+			.then(literal("activate")
+				.then(argument("wildcard", StringArgumentType.greedyString())
+					.suggests((context, builder) -> 
+						SharedSuggestionProvider.suggest(suggestionsActivateWildcard(), builder)
+					)
+					.executes(context -> activateWildcard(
+						context.getSource(), StringArgumentType.getString(context, "wildcard")
+					))
+				)
+			)
+			
+			.then(literal("deactivate")
+				.then(argument("wildcard", StringArgumentType.greedyString())
+					.suggests((context, builder) -> 
+						SharedSuggestionProvider.suggest(suggestionsDeactivateWildcard(), builder)
+					)
+					.executes(context -> deactivateWildcard(
+						context.getSource(), StringArgumentType.getString(context, "wildcard")
+					))
+				)
+			)
+			
+			.then(literal("choose")
+				.requires(source -> NetworkHandlerServer.wasHandshakeSuccessful(source.getPlayer()) 
+									|| source.getEntity() == null)
+				.executes(context -> chooseWildcard(context.getSource()))
+			)
+			
+			.then(literal("finale")
+				.executes(context -> activateFinale(context.getSource()))
+			)
+			
 			.then(literal("effect")
-				.then(literal("dots").executes(context -> effectDots(context.getSource())))
-				.then(literal("makeItWild").executes(context -> effectMakeItWild(context.getSource())))
-		)
-
-		
+				.then(literal("dots")
+					.executes(context -> effectDots(context.getSource()))
+				)
+				.then(literal("makeItWild")
+					.executes(context -> effectMakeItWild(context.getSource()))
+				)
+			);
+	
         dispatcher.register(
             literal("snail")
                 .then(literal("names")
