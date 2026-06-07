108478d0fa1062db61a7b06ee49bc10f378d3215
TheCatKing <dylansheath@outlook.com>
2026-06-07 20:57:01 +0100

    test

--- src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/SimpleLife.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/SimpleLife.java
@@ -122,35 +122,35 @@ public class SimpleLife extends ThirdLife {
 					ItemStack ghastegg = new ItemStack(Items.HAPPY_GHAST_SPAWN_EGG);
 					ItemStack sulfurbucket = new ItemStack(Items.SULFUR_CUBE_BUCKET);
 
-					ghastegg.applyComponents(
-						DataComponentPatch.builder()
-							.set(
-								DataComponents.ENTITY_DATA,
-								CustomData.of(
-									CompoundTag.parse("""
-									{
-										Harness:{
-											id:"minecraft:red_harness",
-											count:1
-										}
-									}
-									""")
-								)
-							)
-							.build()
-					);
+					//ghastegg.applyComponents(
+						//DataComponentPatch.builder()
+							//.set(
+								//DataComponents.ENTITY_DATA,
+								//CustomData.of(
+								//	CompoundTag.parse("""
+								//	{
+									//	Harness:{
+										//	id:"minecraft:red_harness",
+										//	count:1
+									//	}
+									//}
+									//""")
+								//)
+							//)
+							//.build()
+					//);
 
-					CompoundTag sulfurContent = new CompoundTag();
-					sulfurContent.putString("id", "minecraft:tnt");
+					//CompoundTag sulfurContent = new CompoundTag();
+					//sulfurContent.putString("id", "minecraft:tnt");
 
-					sulfurbucket.applyComponents(
-						DataComponentPatch.builder()
-							.set(
-								DataComponents.SULFUR_CUBE_CONTENT,
-								CustomData.of(sulfurContent)
-							)
-							.build()
-					);
+					//sulfurbucket.applyComponents(
+						//DataComponentPatch.builder()
+							//.set(
+							//	DataComponents.SULFUR_CUBE_CONTENT,
+							//	CustomData.of(sulfurContent)
+						//	)
+						//	.build()
+					//);
 
 					offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40),Optional.empty(),sulfurbucket,0,999999,0,0,0));
 					offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40),Optional.empty(),ghastegg,0,999999,0,0,0));
