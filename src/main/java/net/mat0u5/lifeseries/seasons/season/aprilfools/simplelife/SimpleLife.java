3a5eacc96469d214422d3ace6c3e514a221cf0e3
TheCatKing <dylansheath@outlook.com>
2026-06-07 20:24:42 +0100

    Update SimpleLife.java

--- src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/SimpleLife.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/SimpleLife.java
@@ -170,8 +170,12 @@ public class SimpleLife extends ThirdLife {
 					wanderingTraderEntity.overrideOffers(offers);
 					wanderingTraderEntity.addTag("SimpleLifeTrader");
 					return true;
+									}
+								}
 
-            return false;
+								return false;
+							}
+						}
         }
     }
 
