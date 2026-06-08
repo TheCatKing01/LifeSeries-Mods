601df916b9e4b56f9eb97b15935594292321c929
TheCatKing <dylansheath@outlook.com>
2026-06-08 11:30:51 +0100

    Update SimpleLife.java

--- src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/SimpleLife.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/SimpleLife.java
@@ -118,48 +118,8 @@ public class SimpleLife extends ThirdLife {
                     offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(), Items.LAVA_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
 					
 					//? if >= 26.2 {
-					offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(), Items.SULFUR_CUBE_BUCKET, 0, 999999, 0, 0, 0));
-					offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(), Items.HAPPY_GHAST_SPAWN_EGG, 0, 999999, 0, 0, 0));
-					//?}
-					
-					//? if >= 26.2 {
-					
-					//ItemStack ghastegg = new ItemStack(Items.HAPPY_GHAST_SPAWN_EGG);
-					//ItemStack sulfurbucket = new ItemStack(Items.SULFUR_CUBE_BUCKET);
-
-					//ghastegg.applyComponents(
-						//DataComponentPatch.builder()
-							//.set(
-								//DataComponents.ENTITY_DATA,
-								//CustomData.of(
-								//	CompoundTag.parse("""
-								//	{
-									//	Harness:{
-										//	id:"minecraft:red_harness",
-										//	count:1
-									//	}
-									//}
-									//""")
-								//)
-							//)
-							//.build()
-					//);
-
-					//CompoundTag sulfurContent = new CompoundTag();
-					//sulfurContent.putString("id", "minecraft:tnt");
-
-					//sulfurbucket.applyComponents(
-						//DataComponentPatch.builder()
-							//.set(
-							//	DataComponents.SULFUR_CUBE_CONTENT,
-							//	CustomData.of(sulfurContent)
-						//	)
-						//	.build()
-					//);
-
-					//offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40),Optional.empty(),sulfurbucket,0,999999,0,0,0));
-					//offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40),Optional.empty(),ghastegg,0,999999,0,0,0));
-					
+					offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(), Items.SULFUR_CUBE_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
+					offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(), Items.HAPPY_GHAST_SPAWN_EGG.getDefaultInstance(), 0, 999999, 0, 0, 0));
 					//?}
 					
 					offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 1), Optional.empty(), Items.SAND.getDefaultInstance(), 0, 999999, 0, 0, 0));
