28930b5a12308064d78e5f0c42615f52a40f3fdc
TheCatKing <dylansheath@outlook.com>
2026-06-19 16:06:47 +0100

    Update SimpleLife.java

--- src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/SimpleLife.java
+++ src/main/java/net/mat0u5/lifeseries/seasons/season/aprilfools/simplelife/SimpleLife.java
@@ -119,12 +119,10 @@ public class SimpleLife extends ThirdLife {
                     offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(), Items.IRON_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
                     offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(), Items.WATER_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
                     offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(), Items.LAVA_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
-					
-					//? if >= 26.2 {
 
+					//? if >= 26.2 {
 					offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(),
 							Items.SULFUR_CUBE_BUCKET.getDefaultInstance(), 0, 999999, 0, 0, 0));
-
 					//?}
 
                     offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 5), Optional.empty(), Items.GOLD_INGOT.getDefaultInstance(), 0, 999999, 0, 0, 0));
@@ -135,13 +133,11 @@ public class SimpleLife extends ThirdLife {
                     if (rand == 1) offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 32), Optional.empty(), Items.SPRUCE_SAPLING.getDefaultInstance(), 0, 999999, 0, 0, 0));
 					
 					//? if >= 26.2 {
-
 					offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(),
 							Items.HAPPY_GHAST_SPAWN_EGG.getDefaultInstance(), 0, 999999, 0, 0, 0));
 
 					offers.add(new MerchantOffer(new ItemCost(Items.DIRT, 40), Optional.empty(),
 							Items.RED_HARNESS.getDefaultInstance(), 0, 999999, 0, 0, 0));
-
 					//?}
 			
 					wanderingTraderEntity.overrideOffers(offers);
